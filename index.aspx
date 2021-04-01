2756cd08b3bb9232c57076bc553531540fcb8bce
Ben Gao <59623966+bengao98@users.noreply.github.com>
2021-04-01 13:34:07 -0700

    Add files via upload

---
+++ index.aspx
@@ -0,0 +1,112 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="testASPSQLC.webTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css?parameter=1" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table style="width: 100%;">
+                    <tr>
+                        <td class ="c1">&nbsp;Name</td>
+                        <td>&nbsp;
+                            <input type="text" id ="Name" required="" name="name" />
+                        </td>
+                       
+                    </tr>
+                    <tr>
+                        <td class ="c1">&nbsp;Email</td>
+                        <td>&nbsp;
+                            <input type="email" id="Email" required ="" name="email"/>
+                        </td>
+                       
+                    </tr>
+                    <tr>
+                        <td class ="c1">&nbsp;Password</td>
+                        <td>&nbsp;
+                             <input type="password" id="Password" name="password" 
+                             title="Must contain at least two uppercase letters, three special characters, and one number, and at least 8 characters" 
+                             required =""/>
+                             
+                        </td>
+                           
+                    </tr>
+                    
+
+            
+                </table>
+            <div>
+            </div>
+            <br />
+            <label for ="DropDownList1">City: </label>   
+            <asp:DropDownList ID="DropDownList1" runat="server" required="">
+                <asp:ListItem Value="0">--SELECT CITY--</asp:ListItem>
+                <asp:ListItem Value="Surrey">Surrey</asp:ListItem>
+                <asp:ListItem Value="Vancouver">Vancouver</asp:ListItem>
+                <asp:ListItem Value="Richmond">Richmond</asp:ListItem>
+                <asp:ListItem Value="Delta">Delta</asp:ListItem>
+                <asp:ListItem Value="Langley">Langley</asp:ListItem>
+            </asp:DropDownList>
+            <br/>
+            <br />
+            <label for="start">Date:</label>
+
+            <input type="date" id="Date" name="date"
+                    value="2021-04-01"
+                    min="2021-01-01" max="2021-12-31" required=""/>
+            <br/>
+            <br />
+            <label>Gender</label>
+            <br />
+            <input type="radio" id="male" name="gender" value="Male" required=""/>
+            <label for="male">Male</label>
+            <br />
+            <input type="radio" id="female" name="gender" value="Female" />
+            <label for="female">Female</label>
+            <br />
+            <input type="radio" id="other" name="gender" value="Other" />
+            <label for="other">Other</label>
+            <br />
+            <br />
+            <label>What programming languages do you know?</label>
+            <br />
+            <asp:CheckBox ID="CheckBox1" runat="server" Text= "Java" name="java"/>
+            <br />
+            <asp:CheckBox ID="CheckBox2" runat="server" Text= "Python" name="python"/>
+            <br />
+            <asp:CheckBox ID="CheckBox3" runat="server" Text= "Cplus" name="cplus"/>
+            <br />
+            <br />
+            <label for ="UpdateID">ID to update/delete</label>
+            <div class="col-md-1">
+                <input type="text" name ="updateID" id="UpdateID" class="form-control"/>
+            </div>
+            
+            <br />
+                <asp:Button ID="subm" runat="server" Text="Submit" OnClick="submit_Click" />
+                <asp:Button ID="retrieve" runat="server" Text="Retrieve" onClick ="retrieve_Click" formnovalidate =""/>
+                <asp:Button ID="update" runat="server" Text="Update" OnClick="update_Click" formnovalidate =""/>
+                <asp:Button ID="delete" runat="server" Text="Delete" OnClick="delete_Click" formnovalidate =""/>
+                <br />
+                <br />
+                <label for="searchName">Search ID</label>
+            <div class="col-md-1">
+                <input type="text" name="searchID" id ="searchID" class="form-control" />
+            </div>
+                
+                <asp:Button ID="SearchButton" runat="server" Text="Search" onClick ="search_Click" formnovalidate =""/>
+                
+                <asp:GridView ID="GridView1" runat="server" HorizontalAlign="Center" AllowPaging="True" PageSize="5" OnPageIndexChanging ="gridView_PageIndexChanging" AllowSorting="true" OnSorting="gridView_Sorting">
+                <PagerSettings Visible="true" Mode="NumericFirstLast" />
+                </asp:GridView>
+               
+                
+            </div>
+ 
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+
+namespace testASPSQLC
+{
+    public partial class webTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DropDownList1.Items[0].Attributes["disabled"] = "disabled";
+            if (!IsPostBack)
+            {
+                bindData();
+            }
+            
+        }
+
+        protected void bindData()
+        {
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand("Select * from demo", con);
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+            
+
+
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {   
+       
+            string name = Request.Form.Get("name");
+            string eml = Request.Form.Get("email");
+            string pwd = Request.Form.Get("password");
+           
+
+            Regex hasUpper = new Regex(@"(?=.*[A-Z]).(?=.*[A-Z]).{8,}$"); // check for at least 2 upper case
+            Regex hasSpec = new Regex(@"(?=.*\W).(?=.*\W).{8,}$"); // check for at least 3 special characters
+            Regex hasNum = new Regex(@"(?=.*\d).{8,}$"); // check for at least one number
+            
+            if (!hasUpper.IsMatch(pwd) || !hasSpec.IsMatch(pwd) || !hasNum.IsMatch(pwd))
+            {
+                Response.Write("<script>alert('Password must contain at least two uppercase letters, three special characters, and one number, and at least 8 characters')</script>");
+                return;
+            }
+
+            string date = Request.Form.Get("date");
+            string radio = Request.Form.Get("gender");
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+
+            MySqlConnection con = new MySqlConnection(mycon);
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("insert into demo(Name, Email, Password, City, Gender, Date, Java, Python, Cplus) values(@a1, @a2, @a3, @a4, @a5, @a6, @a7, @a8, @a9)", con);
+                cmd.Parameters.AddWithValue("@a1", name);
+                cmd.Parameters.AddWithValue("@a2", eml);
+                cmd.Parameters.AddWithValue("@a3", pwd);
+                cmd.Parameters.AddWithValue("@a4", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@a5", radio);
+                cmd.Parameters.AddWithValue("@a6", date);
+                if (CheckBox1.Checked) // Inputting value of checkboxes into sql
+                {
+                    cmd.Parameters.AddWithValue("@a7", 1);
+                } else
+                {
+                    cmd.Parameters.AddWithValue("@a7", 0);
+                }
+                if (CheckBox2.Checked)
+                {
+                    cmd.Parameters.AddWithValue("@a8", 1);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@a8", 0);
+                }
+                if (CheckBox3.Checked)
+                {
+                    cmd.Parameters.AddWithValue("@a9", 1);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@a9", 0);
+                }
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            } catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                con.Close();
+            }
+            Response.Write("<script>alert('Data saved successfully')</script>");
+          
+         
+        }
+
+        protected void retrieve_Click(object sender, EventArgs e)
+        {
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            DataTable dt = new DataTable();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("Select * from demo", con);
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                con.Close();
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+           
+           
+        }
+
+        protected void update_Click(object sender, EventArgs e)
+        {
+            string id = Request.Form.Get("updateID");
+            string name = Request.Form.Get("name");
+            string eml = Request.Form.Get("email");
+            string pwd = Request.Form.Get("password");
+            Regex hasUpper = new Regex(@"(?=.*[A-Z]).(?=.*[A-Z]).{8,}$"); // check for at least 2 upper case
+            Regex hasSpec = new Regex(@"(?=.*\W).(?=.*\W).{8,}$"); // check for at least 3 special characters
+            Regex hasNum = new Regex(@"(?=.*\d).{8,}$"); // check for at least one number
+
+            if (pwd == "")
+            {
+
+            }
+            else if (!hasUpper.IsMatch(pwd) || !hasSpec.IsMatch(pwd) || !hasNum.IsMatch(pwd))
+            {
+                Response.Write("<script>alert('Password must contain at least two uppercase letters, three special characters, and one number, and at least 8 characters')</script>");
+                return;
+            }
+            string date = Request.Form.Get("date");
+            string radio = Request.Form.Get("gender");
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            DataTable dt = new DataTable();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("Update demo set Name = '" + name + "', Email = '" + eml + "', City = '" + DropDownList1.SelectedItem.Value + "', " +
+                    "Password = '" + pwd + "', Date = '" + date + "', Gender = '" + radio + "' where id = '" + id + "'", con);
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                con.Close();
+            }
+            Response.Write("<script>alert('Data updated successfully')</script>");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+           
+           
+        }
+
+        protected void delete_Click(object sender, EventArgs e)
+        {
+            string id = Request.Form.Get("updateID");       
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            DataTable dt = new DataTable();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("delete from demo where id = '" + id + "'", con);
+                con.Open();
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+                con.Close();
+            }
+            Response.Write("<script>alert('Data deleted successfully')</script>");
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+           
+           
+        }
+
+
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+            string id = Request.Form.Get("searchID");
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+            MySqlDataAdapter da = new MySqlDataAdapter("Select * from demo where id like '" + int.Parse(id) + "%'", mycon);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            
+           
+
+
+        }
+        
+        
+
+        protected void gridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            bindData();
+        }
+
+        protected void gridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            string sortExpr = e.SortExpression;
+            
+            if (SortDir == SortDirection.Ascending)
+            {
+                SortDir = SortDirection.Descending;
+                SortGridView(sortExpr, " DESC");
+            }
+            else
+            {
+                SortDir = SortDirection.Ascending;
+                SortGridView(sortExpr, " ASC");
+            }
+            
+
+
+        }
+
+        private void SortGridView(string sortExpression, string direction)
+        {
+            string mycon = "server =localhost; Uid=root; password = ; persistsecurityinfo = True; database = aldingtest; SslMode = none";
+            MySqlConnection con = new MySqlConnection(mycon);
+            con.Open();
+            MySqlCommand cmd = new MySqlCommand("Select * from demo ", con);
+            DataTable dt = new DataTable();
+            DataView dv = new DataView(dt);
+            dt.Load(cmd.ExecuteReader());
+            dv.Sort = sortExpression + direction;
+            GridView1.DataSource = dv;
+            GridView1.DataBind();
+            con.Close();
+
+        }
+
+        public SortDirection SortDir
+        {
+            get {
+                if (ViewState["sortDirection"] == null) {
+                    ViewState["sortDirection"] = SortDirection.Ascending;
+                }
+                return (SortDirection)ViewState["sortDirection"];
+                }
+            set { 
+                ViewState["sortDirection"] = value;
+            }
+        }
+
+      
+    }
+}
